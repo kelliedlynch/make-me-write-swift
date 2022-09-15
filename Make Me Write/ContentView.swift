c56c64f01f99d4dd11223e2e81a25035b1ad703a
Kellie Lynch <kelliedlynch@gmail.com>
2022-09-15 13:23:10 -0500

    Initial Commit

---
+++ Make Me Write/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Make Me Write
+//
+//  Created by Kellie Lynch on 9/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Make Me Write/Make_Me_WriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Make_Me_WriteApp.swift
+//  Make Me Write
+//
+//  Created by Kellie Lynch on 9/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Make_Me_WriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
